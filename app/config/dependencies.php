a3b03a9ea3eca4c75b25b43d4708909a521b78e1
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2024-10-21 16:04:28 +0200

    Ajout de la dépendance pour ServiceSpectacle

--- app/config/dependencies.php
+++ app/config/dependencies.php
@@ -1,7 +1,12 @@
 <?php
 
 use Psr\Container\ContainerInterface;
+use nrv\core\services\spectacle\ServiceSpectacle;
+use nrv\core\repositoryInterfaces\SpectacleRepositoryInterface;
 
 return [
     
+    ServiceSpectacle::class => function(ContainerInterface $c) {
+        return new ServiceSpectacle($c->get(SpectacleRepositoryInterface::class));
+    }
 ];
