205ee3b80bab60cb3fef7f85139d4172a95acdef
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2024-10-21 16:00:00 +0200

    Ajout de la route pour le Home

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,5 +3,7 @@ declare(strict_types=1);
 
 return function( \Slim\App $app):\Slim\App {
 
+    $app->get('/', callable: nrv\application\actions\HomeAction::class)->setName('home');
+
     return $app;
 };
