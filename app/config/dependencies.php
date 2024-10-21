bfa454757d19783fbb734d45f20d18426a114a15
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2024-10-21 16:17:16 +0200

    Modification dépendance incorrecte ServiceSpectacleInterface

--- app/config/dependencies.php
+++ app/config/dependencies.php
@@ -1,12 +1,13 @@
 <?php
 
+use nrv\core\repositoryInterfaces\ServiceSpectacleInterface;
 use Psr\Container\ContainerInterface;
 use nrv\core\services\spectacle\ServiceSpectacle;
 use nrv\core\repositoryInterfaces\SpectacleRepositoryInterface;
 
 return [
     
-    ServiceSpectacle::class => function(ContainerInterface $c) {
+    ServiceSpectacleInterface::class => function(ContainerInterface $c) {
         return new ServiceSpectacle($c->get(SpectacleRepositoryInterface::class));
     }
 ];
