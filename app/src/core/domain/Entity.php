3422714fea30ed2f01ea0c771b30d247e07fa006
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2024-10-21 14:48:13 +0200

    Ajout de la classe abstraite Entity ainsi que de l'entité Spectacle

---
+++ app/src/core/domain/Entity.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace nrv\core\domain\entities;
+
+abstract class Entity
+{
+
+    protected ?string $ID=null;
+    public function __get(string $name): mixed
+    {
+        return property_exists($this, $name) ? $this->$name : throw new \Exception(static::class . ": Property $name does not exist");
+    }
+
+    public function __set($name, $value): void
+    {
+        if (property_exists($this, $name)) {
+            $this->$name = $value;
+        } else {
+            throw new \Exception(static::class . ": Property $name does not exist");
+        }
+    }
+
+    public function setID(string $ID): void
+    {
+        $this->ID = $ID;
+    }
+
+    public function getID(): ?string
+    {
+        return $this->ID;
+    }
+
+}
