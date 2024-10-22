4e8cdf8d70bce8de3180ac0f3a0aaf531e044434
Matheo_grdj <126459096+grandj161u@users.noreply.github.com>
2024-10-22 09:44:58 +0200

    ajout des dépendances des pdo

--- app/config/dependencies.php
+++ app/config/dependencies.php
@@ -6,6 +6,46 @@ use nrv\core\services\spectacle\ServiceSpectacle;
 use nrv\core\repositoryInterfaces\SpectacleRepositoryInterface;
 
 return [
+
+    'artiste.pdo' => function (ContainerInterface $c) {
+        $config = parse_ini_file(__DIR__ . '/artiste.db.ini');
+        $dsn = "{$config['driver']}:host={$config['host']};dbname={$config['database']}";
+        $user = $config['username'];
+        $password = $config['password'];
+        return new PDO($dsn, $user, $password);
+    },
+
+    'billet.pdo' => function (ContainerInterface $c) {
+        $config = parse_ini_file(__DIR__ . '/billet.db.ini');
+        $dsn = "{$config['driver']}:host={$config['host']};dbname={$config['database']}";
+        $user = $config['username'];
+        $password = $config['password'];
+        return new PDO($dsn, $user, $password);
+    },
+
+    'spectacle.pdo' => function (ContainerInterface $c) {
+        $config = parse_ini_file(__DIR__ . '/spectacle.db.ini');
+        $dsn = "{$config['driver']}:host={$config['host']};dbname={$config['database']}";
+        $user = $config['username'];
+        $password = $config['password'];
+        return new PDO($dsn, $user, $password);
+    },
+
+    'lieu.pdo' => function (ContainerInterface $c) {
+        $config = parse_ini_file(__DIR__ . '/lieu.db.ini');
+        $dsn = "{$config['driver']}:host={$config['host']};dbname={$config['database']}";
+        $user = $config['username'];
+        $password = $config['password'];
+        return new PDO($dsn, $user, $password);
+    },
+
+    'utilisateur.pdo' => function (ContainerInterface $c) {
+        $config = parse_ini_file(__DIR__ . '/utilisateur.db.ini');
+        $dsn = "{$config['driver']}:host={$config['host']};dbname={$config['database']}";
+        $user = $config['username'];
+        $password = $config['password'];
+        return new PDO($dsn, $user, $password);
+    },
     
     ServiceSpectacleInterface::class => function(ContainerInterface $c) {
         return new ServiceSpectacle($c->get(SpectacleRepositoryInterface::class));
