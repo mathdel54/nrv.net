a4b9aa735b713c40393e7036a6d69a1703e71d8e
Matheo_grdj <126459096+grandj161u@users.noreply.github.com>
2024-10-22 17:14:34 +0200

    correction finale (normalement)

--- app/config/routes.php
+++ app/config/routes.php
@@ -7,5 +7,7 @@ return function( \Slim\App $app):\Slim\App {
 
     $app->get('/spectacles', callable: nrv\application\actions\ListeSpectacleAction::class)->setName('spectacles');
 
+    $app->get('/spectacles/{ID_Spectacle}/artistes', callable: nrv\application\actions\ArtistesBySpectacleAction::class)->setName('artistesBySpectacles');
+
     return $app;
 };
