8e2024522a11292a577af287d935179f12e756ea
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2024-10-23 09:40:55 +0200

    Déplaceement du Middleware Cors de routes.php à bootstrap.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -8,24 +8,19 @@ return function (\Slim\App $app): \Slim\App {
 
     $app->get('/', callable: nrv\application\actions\HomeAction::class)->setName('home');
 
-    $app->get('/spectacles', callable: nrv\application\actions\ListeSpectacleAction::class)->setName('spectacles')
-        ->add(new Cors());
+    $app->get('/spectacles', callable: nrv\application\actions\ListeSpectacleAction::class)->setName('spectacles');
 
     $app->get('/spectacles/{ID_Spectacle}/artistes', callable: nrv\application\actions\ArtistesBySpectacleAction::class)
-        ->setName('artistesBySpectacles')
-        ->add(new Cors());
+        ->setName('artistesBySpectacles');
 
     $app->get('/soiree/{id}', callable: nrv\application\actions\DetailSoireeAction::class)
-        ->setName('soiree')
-        ->add(new Cors());
+        ->setName('soiree');
 
     $app->get('/soirees/{id}/spectacles', callable: nrv\application\actions\ListeSpectacleSoireeAction::class)
-        ->setName('spectaclesSoiree')
-        ->add(new Cors());
+        ->setName('spectaclesSoiree');
 
     $app->get('/spectacles/{ID_Spectacle}/soiree', callable: nrv\application\actions\SoireeBySpectacleAction::class)
-        ->setName('soireeBySpectacle')
-        ->add(new Cors());
+        ->setName('soireeBySpectacle');
 
     return $app;
 };
