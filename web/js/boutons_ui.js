a480dce646fb2e45390e4228e315b73f35830ea2
Matheo_grdj <126459096+grandj161u@users.noreply.github.com>
2024-10-24 15:15:11 +0200

    continuité du sass (liste des spectacles mise en forme)

--- web/js/boutons_ui.js
+++ web/js/boutons_ui.js
@@ -1,18 +1,18 @@
 import Handlebars from 'handlebars';
-import {loadSpectaclesParStyle} from "./spectaclesLoader";
-import {display_spectacles} from "./spectacles_ui";
+import { loadSpectaclesParStyle } from "./spectaclesLoader";
+import { display_spectacles } from "./spectacles_ui";
 
 const source = document.getElementById('buttonsTemplate').innerHTML;
 const template = Handlebars.compile(source);
 export function display_buttons(styles) {
 
-    document.getElementById('templateBoutons').innerHTML = template({styles: styles});
+    document.getElementById('templateBoutons').innerHTML = template({ styles: styles });
 
     //On rajoute un evenement sur les styles
     document.querySelectorAll('.filtreStyle').forEach(style => {
         style.addEventListener('click', async () => {
             let spectacles = await loadSpectaclesParStyle(style.dataset.style);
-            display_spectacles(spectacles,  style.dataset.style);
+            display_spectacles(spectacles, style.dataset.style);
         });
     })
 }
